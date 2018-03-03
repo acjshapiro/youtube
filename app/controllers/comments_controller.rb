395b509f9a0819e36877599f120fb978492643da
Alex Shapiro <acjshapiro@gmail.com>
2018-03-03 15:29:57 -0700

    resolve

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,56 +1,31 @@
 class CommentsController < ApplicationController
   before_action :set_movie
-  before_action :set_comment, only: [:show, :edit, :update, :destroy]
-
-  def index
-    @comments = @movie.comments
-  end
-
-  def show
-  end
 
   def new
-    @comment = @movie.comments.new
-
-  end
-
-  def edit
+    @comment = Comment.new
   end
 
   def create
-    @comment = @movie.comments.new(comment_params)
-    if @comment.save
-      redirect_to [@movie, @comment]
-    else
-      render :new
-    end
-  end
+      @comment = @movie.comments.new(comment_params)
+      @comment.user_id = current_user.id
 
-  def update
-    if @comment.update(comment_params)
-      redirect_to [@movie, @comment]
-    else
-      render :edit
+      if @comment.save
+        redirect_to movie_path(@movie)
+      else
+        render :new
+      end
     end
-  end
-
   def destroy
     @comment.destroy
-    redirect_to movie_comments_path
+    redirect_to movie_path(@movie)
+  end 
+
+private
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 
-  private
-
-      def set_movie
-        @movie = Movie.find(params[:movie_id])
-      end
-
-      def set_comment
-        @comment = Comment.find(params[:id])
-      end
-
-      def comment_params
-        params.require(:comment).permit(:user_id, :movie_id, :body)
-      end   
-
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
 end
