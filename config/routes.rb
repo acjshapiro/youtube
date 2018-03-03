0a8e1de0036687fde023ef6014e1848d4c0b9270
Alex Shapiro <acjshapiro@gmail.com>
2018-03-03 13:03:16 -0700

    movies controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,12 @@ Rails.application.routes.draw do
   devise_for :users
   root 'movies#index'
 
-  resources :movies
+  resources :movies do
+    resources :comments
+  end
+
+  resources :users do
+    resources:comments
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
